fa942de6cd50a9ec4941fd63e3debf91f06ee868
psotou <pascualsu@gmail.com>
2020-11-04 19:36:56 -0300

    update que incluye el spread

--- main.go
+++ main.go
@@ -20,22 +20,25 @@ func main() {
 	market, _ := budaClient.GetTickerByMarket(crypto)
 	volume, _ := budaClient.GetVolumeByMarket(crypto)
 
-	var oneDayVariationStr string = market.PriceVariation24H
-	var sevenDayVariationStr string = market.PriceVariation7D
+	//var oneDayVariationStr string = market.PriceVariation24H
+	//var sevenDayVariationStr string = market.PriceVariation7D
 
-	oneDayVariation, _ := strconv.ParseFloat(oneDayVariationStr, 64)
-	sevenDayVariation, _ := strconv.ParseFloat(sevenDayVariationStr, 64)
+	oneDayVariation, _ := strconv.ParseFloat(market.PriceVariation24H, 64)
+	sevenDayVariation, _ := strconv.ParseFloat(market.PriceVariation7D, 64)
+	minAsk, _ := strconv.ParseFloat(market.MinAsk[0], 64)
+	maxBid, _ := strconv.ParseFloat(market.MaxBid[0], 64)
 
+	var spread float64 = minAsk - maxBid
 	var oneDayVariationPercent float64 = oneDayVariation * 100
 	var sevenDayVariationPercent float64 = sevenDayVariation * 100
 
 	fmt.Printf("Precio última orden ejecutada  $ %s\n", market.LastPrice[0])
 	fmt.Printf("Menor precio de venta          $ %s\n", market.MinAsk[0])
 	fmt.Printf("Máximo precio de compra        $ %s\n", market.MaxBid[0])
+	fmt.Printf("Spread                         $ %.1f\n", spread)
 	fmt.Printf("Variación últimas 24h          %2.2f %%\n", oneDayVariationPercent)
 	fmt.Printf("Variación últimos 7d           %2.2f %%\n", sevenDayVariationPercent)
 	fmt.Printf("Volumen criptomenda            %s\n", market.Volume[0])
-
 	fmt.Printf("Volumen venta últimas 24h      %s\n", volume.AskVolumen24h[0])
 	fmt.Printf("Volumen compra últimas 24h     %s\n", volume.BidVolumen24h[0])
 	// fmt.Printf("Volumen venta últimos 7d       %s\n", volume.AskVolumen7d[0])
