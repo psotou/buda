47cfe49c8d405121434a080a02347106be9dc029
psotou <pascualsu@gmail.com>
2020-11-13 12:25:02 -0300

    update on the way I show the daily volume variation

--- main.go
+++ main.go
@@ -26,13 +26,19 @@ func main() {
 	sevenDayVariation, _ := strconv.ParseFloat(market.PriceVariation7D, 64)
 	minAsk, _ := strconv.ParseFloat(market.MinAsk[0], 64)
 	maxBid, _ := strconv.ParseFloat(market.MaxBid[0], 64)
+    volVenta, _ := strconv.ParseFloat(volume.AskVolumen24h[0], 64)
+    volCompra, _ := strconv.ParseFloat(volume.BidVolumen24h[0], 64)
 
+    var volSuma float64 = volVenta + volCompra
+    var porcentajeVolVenta float64 = (volVenta / volSuma) * 100
+    var porcentajeVolCompra float64 = (volCompra / volSuma) * 100
 	var spread float64 = minAsk - maxBid
 	var oneDayVariationPercent float64 = oneDayVariation * 100
 	var sevenDayVariationPercent float64 = sevenDayVariation * 100
+    var criptomoneda string = strings.ToUpper(os.Args[1])
 
     fmt.Println("---------------------------------------------")
-    fmt.Printf("                MERCADO %s-CLP\n", strings.ToUpper(os.Args[1]))
+    fmt.Printf("                MERCADO %s-CLP\n", criptomoneda)
     fmt.Println("---------------------------------------------")
 
 	fmt.Printf("Precio última orden ejecutada  $ %s\n", market.LastPrice[0])
@@ -41,9 +47,6 @@ func main() {
 	fmt.Printf("SPREAD                         $ %.1f\n", spread)
 	fmt.Printf("Variación últimas 24h          %2.2f %%\n", oneDayVariationPercent)
 	fmt.Printf("Variación últimos 7d           %2.2f %%\n", sevenDayVariationPercent)
-	// fmt.Printf("Volumen criptomenda            %s\n", market.Volume[0])
-	fmt.Printf("Volumen venta últimas 24h      %s\n", volume.AskVolumen24h[0])
-	fmt.Printf("Volumen compra últimas 24h     %s\n", volume.BidVolumen24h[0])
-	// fmt.Printf("Volumen venta últimos 7d       %s\n", volume.AskVolumen7d[0])
-	// fmt.Printf("Volumen compra últimos 7d      %s\n", volume.BidVolumen7d[0])
+    fmt.Printf("Volumen venta últimas 24h      %3.2f (%2.1f %%)\n", volVenta, porcentajeVolVenta)
+    fmt.Printf("Volumen compra últimas 24h     %3.2f (%2.1f %%)\n", volCompra, porcentajeVolCompra)
 }
